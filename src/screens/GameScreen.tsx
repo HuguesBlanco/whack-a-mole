2a565e4fdfc19f146f86d1d3b23aa6fba97514a9
Hugues Blanco <contact@huguesblanco.com>
2025-01-18 21:27:57 +0100

    Implement generation of mole grid data at a random interval
    In GameScreen component, implemented the first version of mole grid data generation, where the generation occurs at a random interval.

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import hammerCursor from '../assets/hammer-cursor-small.png';
 import { generateMolesGridData } from '../libs/gridGeneration';
@@ -7,12 +7,30 @@ import { AppDispatch, RootState } from '../store/store';
 import { MolesGridData } from '../types';
 import MolesGrid from '../ui/MolesGrid';
 
+function getRandomDelay(): number {
+  return Math.floor(Math.random() * (2000 - 200 + 1)) + 200;
+}
+
 function GameScreen(): React.JSX.Element {
   const gameState = useSelector((state: RootState) => state.game);
   const dispatch = useDispatch<AppDispatch>();
 
-  const molesGridData: MolesGridData = useMemo(generateMolesGridData, []);
+  const [molesGridData, setMolesGridData] = useState<MolesGridData>(
+    generateMolesGridData(),
+  );
 
+  // Generate grid data at random interval.
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setMolesGridData(generateMolesGridData());
+    }, getRandomDelay());
+
+    return (): void => {
+      clearTimeout(timeoutId);
+    };
+  }, [molesGridData]);
+
+  // Run game timer.
   useEffect(() => {
     const intervalId = setInterval(() => {
       dispatch(decrementTimer());
