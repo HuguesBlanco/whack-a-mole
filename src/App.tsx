47f7c2331bf767a3fdd9798fed21f8a0d90b002d
Hugues Blanco <contact@huguesblanco.com>
2025-01-19 20:47:11 +0100

    Move font file in style folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import './font.css';
 import GameScreen from './screens/GameScreen';
 import HomeScreen from './screens/HomeScreen';
 import ScoreScreen from './screens/ScoreScreen';
 import { RootState } from './store/store';
+import './styles/font.css';
 
 function App(): React.JSX.Element {
   const gameState = useSelector((state: RootState) => state.game);
