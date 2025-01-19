172ac570e71d44ddb6082f33b25fcfa187ba08c1
Hugues Blanco <contact@huguesblanco.com>
2025-01-19 19:48:21 +0100

    Add green color value

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,4 +1,4 @@
 export const COLOR_PURPLE = '#190b71';
 export const COLOR_YELLOW = '#F0C97A';
-export const COLOR_GREEN = '';
+export const COLOR_GREEN = '#0DC458';
 export const COLOR_RED = '#f5044a';
