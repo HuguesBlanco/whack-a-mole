55ec951ee2255aca4a1943d8783fdf16ea8a9d2e
Hugues Blanco <contact@huguesblanco.com>
2025-01-27 19:01:31 +0100

    Add tests for HomeTemplate components

---
+++ src/ui/templates/HomeTemplate.test.tsx
@@ -0,0 +1,43 @@
+import '@testing-library/jest-dom/vitest';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+import { COLOR_YELLOW } from '../../styles/colors';
+import HomeTemplate from './HomeTemplate';
+
+describe('HomeTemplate component', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  const mockOnClickStart = vi.fn();
+
+  it('should render the Title component with the correct text', () => {
+    render(<HomeTemplate onClickStart={mockOnClickStart} />);
+
+    const titleElement = screen.getByText('Whack a mole');
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  it('should call the onClickStart callback when the button is clicked', () => {
+    render(<HomeTemplate onClickStart={mockOnClickStart} />);
+
+    const buttonElement = screen.getByRole('button', { name: 'Start Game' });
+
+    fireEvent.click(buttonElement);
+
+    expect(mockOnClickStart).toHaveBeenCalledTimes(1);
+  });
+
+  it('should render a yellow overlay on top of the game background', () => {
+    const { container } = render(
+      <HomeTemplate onClickStart={mockOnClickStart} />,
+    );
+
+    const overlayDiv = container.firstChild?.firstChild;
+
+    expect(overlayDiv).toHaveStyle({
+      backgroundColor: COLOR_YELLOW,
+      opacity: '0.8',
+    });
+  });
+});
